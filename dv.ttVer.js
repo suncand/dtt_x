8b68e4a4ca5a02becda09c27c54e0a23c5563cb5
Louis Shi <909192524@qq.com>
2021-01-12 16:31:10 +0800

    add js file

---
+++ dv.ttVer.js
@@ -0,0 +1,36 @@
+{ version: '2.0.02',
+  '2021.2.10': 
+   { n01: 
+		{ CHS: '修改了检查Ib最大值的一个bug', 
+          CHC: null, 
+		  ENG: null },
+     n02: 
+		{ CHS: null, 
+		  CHC: null, 
+		  ENG: null },
+     n03: 
+		{ CHS: null, 
+		  CHC: null, 
+		  ENG: null } 
+   },
+  '2021.1.10': 
+   { n01: 
+		{ CHS: null, 
+		  CHC: null, 
+		  ENG: null },
+     n02: 
+	    { CHS: null, 		  
+		  CHC: null, 
+		  ENG: null },
+     n03: 
+		{ CHS: null, 
+		  CHC: null, 
+		  ENG: null } 
+   },
+  wip: 
+	{ n01: 
+		{ CHS: '修改驱动兼容老版', 
+		  CHC: null, 
+		  ENG: null } 
+	} 
+}
