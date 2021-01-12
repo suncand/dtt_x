aec610482c14ba6f872656ca28e5822539acf22c
Louis Shi <909192524@qq.com>
2021-01-12 18:49:48 +0800

    adjust section

--- dv.ttVer.js
+++ dv.ttVer.js
@@ -3,36 +3,36 @@
 	{'2021.02.10': 
 	   { 'n01': 
 			{ 'CHS': '修改了检查Ib最大值的一个bug', 
-			  'CHC': null, 
-			  'ENG': null },
+			  'CHC': None, 
+			  'ENG': None },
 		 'n02': 
-			{ 'CHS': null, 
-			  'CHC': null, 
-			  'ENG': null },
+			{ 'CHS': None, 
+			  'CHC': None, 
+			  'ENG': None },
 		 'n03': 
-			{ 'CHS': null, 
-			  'CHC': null, 
-			  'ENG': null } 
+			{ 'CHS': None, 
+			  'CHC': None, 
+			  'ENG': None } 
 	   },
 	  '2021.01.10': 
 	   { 'n01': 
-			{ 'CHS': null, 
-			  'CHC': null, 
-			  'ENG': null },
+			{ 'CHS': None, 
+			  'CHC': None, 
+			  'ENG': None },
 		 'n02': 
-			{ 'CHS': null, 
-			  'CHC': null, 
-			  'ENG': null },
+			{ 'CHS': None, 
+			  'CHC': None, 
+			  'ENG': None },
 		 'n03': 
-			{ 'CHS': null, 
-			  'CHC': null, 
-			  'ENG': null } 
+			{ 'CHS': None, 
+			  'CHC': None, 
+			  'ENG': None } 
 	   }
 	},
   'wip': 
 	{ 'n01': 
 		{ 'CHS': '修改驱动兼容老版', 
-		  'CHC': null, 
-		  'ENG': null } 
+		  'CHC': None, 
+		  'ENG': None } 
 	} 
 }
